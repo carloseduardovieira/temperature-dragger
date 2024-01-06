d33ae9fe27da7dd5b26862a6b99be718fbdd7bae
Cvieira <dullvieira@gmail.com>
2024-01-06 17:58:19 +0000

    [feature] temperature dragger: added temperature display

--- src/app/core/temperature-dragger/temperature-dragger.component.ts
+++ src/app/core/temperature-dragger/temperature-dragger.component.ts
@@ -1,27 +1,61 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   Input,
-  OnInit,
+  Renderer2,
+  ViewChild,
 } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-temperature-dragger',
-  templateUrl: './temperature-dragger.component.html',
+  template: `
+    <div class="valve">
+      <div class="valve--min-marker" #minMarker></div>
+      <div class="valve--max-marker" #maxMarker></div>
+      <div class="valve__circle">
+        <div class="valve__circle--temperature-marker" #temperatureMarker></div>
+      </div>
+    </div>
+
+    <div class="temperature-display">
+      <span class="temperature-display--label">{{ current }}°C</span>
+    </div>
+  `,
   styleUrls: ['./temperature-dragger.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [IonicModule],
 })
-export class TemperatureDraggerComponent implements OnInit {
-  @Input() min: number | undefined;
-  @Input() max: number | undefined;
-  @Input() current: number | undefined;
+export class TemperatureDraggerComponent implements AfterViewInit {
+  @ViewChild('minMarker') minMarker: ElementRef | undefined;
+  @ViewChild('maxMarker') maxMarker: ElementRef | undefined;
+  @ViewChild('temperatureMarker') temperatureMarker: ElementRef | undefined;
 
-  constructor() {}
+  @Input() min: number = 0;
+  @Input() max: number = 100;
+  @Input() current: number = 16;
 
-  ngOnInit() {
-    console.log('min', this.min);
-    console.log('max', this.max);
-    console.log('current', this.current);
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    if (this.min > this.max) {
+      console.error('the minimum value must be less than the maximum');
+      return;
+    }
+    this.initMarkerLimit(this.minMarker as ElementRef, this.min);
+    this.initMarkerLimit(this.maxMarker as ElementRef, this.max);
+    this.initMarkerLimit(this.temperatureMarker as ElementRef, this.current);
+  }
+
+  initMarkerLimit(marker: ElementRef, value: number) {
+    const minDeg = Math.floor(Math.floor(value * 360) / 100) - 90;
+    this.renderer.setStyle(
+      marker.nativeElement,
+      'transform',
+      `rotate(${minDeg}deg)`
+    );
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,4 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: 'home.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
-  constructor() {}
-}
+export class HomePage {}
